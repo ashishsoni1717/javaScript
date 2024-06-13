885a634aea37f31d8cac2673cb3ba91ce882f401
ashishsoni1717 <45263065+ashishsoni1717@users.noreply.github.com>
2024-06-13 18:47:55 +0000

    convertionofDatatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "ashish"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33"=> 33
+// "33abc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn = "ashish"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+// "" => false
+// "ashish"=> true
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
